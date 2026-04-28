14f0695e8d7f67e35d2c02a43900ace95de16d4b
Rupeshkumar663 <rupeshsamrat08@gmail.com>
2026-04-28 21:25:09 +0530

    postgres

--- postgres-userApi/App.js
+++ postgres-userApi/App.js
@@ -1,12 +1,16 @@
 import express from "express"
 import dotenv from "dotenv"
-import { createemployee } from "./employee.js"
+import { createemployee, deleteemployee, getalls, getemployeebyid, updateemployees } from "./employee.js"
 const app=express()
 dotenv.config()
 const PORT=process.env.PORT ||6000
 app.use(express.json())
 
-app.post('/adduser',createemployee);
+app.post('/create',createemployee);
+app.get('/allgets',getalls)
+app.get('/:id',getemployeebyid)
+app.put('/:id',updateemployees)
+app.delete('/:id',deleteemployee)
 /*app.get("/",(req,res)=>{
     res.send("mubarak ho tm home page pe ho")
 }) */
--- postgres-userApi/employee.js
+++ postgres-userApi/employee.js
@@ -1,5 +1,5 @@
 import pkg from "pg";
-import dotenv, { config } from "dotenv"
+import dotenv from "dotenv"
 dotenv.config()
 const { Pool } = pkg;
 const pool=new Pool({
@@ -10,13 +10,14 @@ const pool=new Pool({
     port:5432
 })
 
+
 export const createemployee=async(req,res)=>{
    try{
       const {name,email}=req.body 
       if(!name ||!email){
         return res.status(404).json({message:"please fill all fields"});
       }
-      const result=await pool.query('INSERT INTO employees (name,email) VALUES ($1,$2) RETURNING *',[name, email])
+      const result=await pool.query('INSERT INTO employees (name,email) VALUES ($1,$2) RETURNING *',[name,email])
       res.status(200).json({
         message:"data fetch successfully ",
         data:result.rows[0]
@@ -27,4 +28,62 @@ export const createemployee=async(req,res)=>{
   }
 }
 
+export const getalls=async(req,res)=>{
+  try {
+      const result=await pool.query('SELECT * FROM employees')
+      if(!result){
+        return res.status(404).json({message:"data not found"});
+      }
+      return res.status(200).json({
+        message:"all data gets successfully",
+        data:result.rows
+      })
+  } catch(error){
+    console.log(error)
+    res.status(500).json({message:"server error"});
+  }
+}
 
+export const getemployeebyid=async(req,res)=>{
+  try {
+    const id=parseInt(req.params.id)
+    const result=await pool.query(`SELECT * FROM employees WHERE id=$1`,[id]);
+     if(!result){
+        return res.status(404).json({message:"data not found"});
+       }
+       return res.status(200).json({
+        message:"get data successfully",
+        data:result.rows[0]
+      })
+  } catch(error){
+    console.log(error)
+    res.status(500).json({message:"server error"});
+  }
+}
+
+export const updateemployees=async(req,res)=>{
+  try {
+    const id=parseInt(req.params.id)
+    const {name,email}=req.body
+     if(!name ||!email){
+        return res.status(404).json({message:"no update"});
+      }
+     await pool.query(`UPDATE employees SET name=$1 ,email=$2 WHERE id=$3`,[name,email,id]);
+      return res.status(200).json({message:"update successfully"})
+  } catch(error){
+    console.log(error)
+    res.status(500).json({message:"server error"});
+  }
+}
+
+
+export const deleteemployee=async(req,res)=>{
+  try {
+    const id=parseInt(req.params.id)
+    await pool.query(`DELETE  FROM employees WHERE id=$1`,[id]);
+    return res.status(200).json({message:"delete  successfully"})
+  } catch(error){
+    console.log(error)
+    res.status(500).json({message:"server error"});
+  }
+}
