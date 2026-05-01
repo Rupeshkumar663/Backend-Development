5cd6887b96f3e9430dc729c84039dacd3002992c
Rupeshkumar663 <rupeshsamrat08@gmail.com>
2026-05-01 21:30:59 +0530

    prisma post table and comment table

--- prisma_folder/index.js
+++ prisma_folder/index.js
@@ -1,13 +1,14 @@
 import express from "express"
 import dotenv from "dotenv"
 import userRoute from "./routes/user.route.js"
+import postRoute from "./routes/post.route.js"
 const app=express()
 dotenv.config()
 const PORT=process.env.PORT ||6000
 app.use(express.json())
 
 app.use("/users",userRoute)
- 
+app.use("/posts",postRoute)
 app.listen(PORT,()=>{
     console.log(`server is running on ${PORT}`)
 })
